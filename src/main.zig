c6c0b8e569e20ca816b4584be985f593887b70dd
SoulThy <74660769+SoulThy@users.noreply.github.com>
2026-09-09 12:31:19 +0200

    feat: add command line arguments for kelvins

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,25 @@ const std = @import("std");
 const wl = @import("wayland.zig");
 const linux = std.os.linux;
 
+const usage =
+    \\Usage: ./melatonina [options]
+    \\
+    \\Options:
+    \\  -k [k]elvin_temperature
+    \\
+    \\Example: ./melatonina -k 4500
+    \\
+    \\Kelvin tips:
+    \\ 5000K █░░░░░░░░░ Almost neutral
+    \\ 4000K ███░░░░░░░ Slightly orange
+    \\ 3000K ██████░░░░ Moderatly orange
+    \\ 2000K █████████░ Strong orange
+    \\
+;
+
 pub fn main(init: std.process.Init) !void {
+    const kelvin = args_get_kelvin(init.minimal.args);
+
     const fd = try wl.display_connect(init.environ_map);
     defer _ = linux.close(fd);
 
@@ -14,9 +32,23 @@ pub fn main(init: std.process.Init) !void {
     try wl.wait_for_sync(&client);
 
     try init_gamma(&client);
-    client.gamma_control.?.table.set_kelvin(3000);
+    client.gamma_control.?.table.set_kelvin(kelvin);
     try wl.zwlr_gamma_control_v1_set_gamma(&client);
     _ = try init.io.sleep(.fromSeconds(3), .awake);
+}
+
+fn args_get_kelvin(args: std.process.Args) u16 {
+    var it = args.iterate();
+    _ = it.next();
+    
+    const option = it.next() orelse fatal(usage, .{});
+    if (!std.mem.eql(u8, option, "-k"))
+        fatal(usage, .{});
+
+    const value = it.next() orelse fatal(usage, .{});
+    const kelvin = std.fmt.parseInt(u16, value, 10) 
+        catch fatal("Error when parsing temperature: '{s}'", .{value});
+    return kelvin;
 }
 
 fn init_gamma(client: *wl.WaylandClient) !void {
@@ -38,4 +70,9 @@ fn init_gamma(client: *wl.WaylandClient) !void {
         gamma_table.data[1][i] = v;
         gamma_table.data[2][i] = v;
     }
+}
+
+fn fatal(comptime format: []const u8, args: anytype) noreturn {
+    std.debug.print(format, args);
+    std.process.exit(1);
 }
